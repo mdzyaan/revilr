cb4cf1a967c1dde78beb30152ac47b9d6455d4d2
mdzyaan <mdzyaan@gmail.com>
2018-11-08 23:00:06 +0530

    api key saved

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
 export const key = process.env.API_KEY;
 console.log('config key',process.env.API_KEY)
+console.log(process.env.FIREBASE_API_KEY);
